7662e66a74930cf1775a0b37d20ea5155ad4a129
nomaderavi-stack <nomaderavi@gmail.com>
2026-07-03 02:08:13 -0400

    Atualizar o app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,7 +1,17 @@
 const botao = document.getElementById("btnAnalisar");
 const resultado = document.getElementById("resultado");
+const usuario = document.getElementById("usuario");
 
 botao.addEventListener("click", () => {
-  resultado.innerHTML =
-    "<h2>✅ Sistema funcionando!</h2><p>O Lumos Follow está conectado ao JavaScript.</p>";
+
+    if (usuario.value.trim() === "") {
+        resultado.innerHTML = "<p>⚠️ Digite um @usuário.</p>";
+        return;
+    }
+
+    resultado.innerHTML = `
+        <h2>🔍 Analisando perfil...</h2>
+        <p>Usuário: <strong>${usuario.value}</strong></p>
+        <p>Em breve o Lumos Follow mostrará quem não segue você de volta.</p>
+    `;
 });
