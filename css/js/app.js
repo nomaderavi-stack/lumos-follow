01c2eb1645a65d40033a65c4a4683dadc480f1c8
nomaderavi-stack <nomaderavi@gmail.com>
2026-07-03 01:15:17 -0400

    Atualizar o app.js
    Adiciona interação do botão

--- css/js/app.js
+++ css/js/app.js
@@ -1,1 +1,9 @@
-console.log("Lumos Seguir iniciado!");
+const botao = document.getElementById("btnAnalisar");
+const resultado = document.getElementById("resultado");
+
+botao.addEventListener("click", () => {
+    resultado.innerHTML = `
+        <h2>✅ Sistema funcionando!</h2>
+        <p>O Lumos Follow está conectado ao JavaScript.</p>
+    `;
+});
