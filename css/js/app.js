d587a9832b3f4c7f9889ac05e6e5af92796b1083
nomaderavi-stack <nomaderavi@gmail.com>
2026-07-03 01:52:05 -0400

    Atualizar o app.js

--- css/js/app.js
+++ css/js/app.js
@@ -2,8 +2,8 @@ const botao = document.getElementById("btnAnalisar");
 const resultado = document.getElementById("resultado");
 
 botao.addEventListener("click", () => {
-    resultado.innerHTML = `
-        <h2>✅ Sistema funcionando!</h2>
-        <p>O Lumos Follow está conectado ao JavaScript.</p>
-    `;
+  resultado.innerHTML = `
+    <h2>✅ Sistema funcionando!</h2>
+    <p>O Lumos Follow está conectado ao JavaScript.</p>
+  `;
 });
